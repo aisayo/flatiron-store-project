3400cb1c25736de84d69b228d960255261f3f548
github username <github email address>
2017-11-15 11:30:15 -0800

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,11 @@
 class CartsController < ApplicationController
 
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
+  def checkout
+    @cart = Cart.find(params[:id])
+    redirect_to cart_path(@cart)
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,44 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
 
+  def status
+
+  end
+
+  def total
+    total = 0
+    self.line_items.each do |li|
+        total += li.item.price * li.quantity
+    end
+    total
+  end
+
+  def add_item(item_id)
+    line_item = self.line_items.find_by(item_id: item_id)
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = self.line_items.build(item_id: item_id)
+    end
+    line_item
+  end
+
+  def checkout
+   self.status = "submitted"
+   self.clear_inventory
+   self.user.current_cart_id = nil
+   self.user.save
+   self.save
+ end
+
+ def clear_inventory
+   if self.status == "submitted"
+     self.line_items.each do |line_item|
+       line_item.item.inventory -= line_item.quantity
+       line_item.item.save
+     end
+   end
+ end
 end
